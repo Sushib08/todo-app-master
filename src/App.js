7d75f71818234cf9727a637a7ef0d4b8a3e97e54
Sushib08 <94189204+Sushib08@users.noreply.github.com>
2022-01-10 19:03:08 +0100

    feat(Todo App):
    Add append()
    try to add checkbox()

--- src/App.js
+++ src/App.js
@@ -9,44 +9,28 @@ function App(props) {
   const [isConnected, setisConnected] = useState(true);
 
   const addCallback = () => {
-    setisConnected(() => true);
+    setisConnected(() => false);
   };
 
   const delCallback = () => {
-    setisConnected(() => false);
+    setisConnected(() => true);
   };
 
   let [isColor, setisColor] = useState("#f5f5f5")
 
-  const red = () => {
-    setisColor("#ff1b1b")
-  }
-
-  const blue = () => {
-    setisColor("#1be8ff")
-  }
-
-  const green = () => {
-    setisColor("#1bff49")
-  }
-
-  const yellow = () => {
-    setisColor("#ffef1b")
-  }
-
   return (
     <div className="container">
       <div className="colors">
-        <button className="red" onClick={red}></button>
-        <button className="blue" onClick={blue}></button>
-        <button className="green" onClick={green}> </button>
-        <button className="yellow" onClick={yellow}></button>
+        <button className="red"  onClick={() => setisColor('#ff1b1b')}></button>
+        <button className="blue" onClick={() => setisColor('#1be8ff')}></button>
+        <button className="green" onClick={() => setisColor('#1bff49')}> </button>
+        <button className="yellow" onClick={() => setisColor('#ffef1b')}></button>
       </div>
       <div className="btnCallback">
-        <button className="callback" onClick={addCallback}>
+        <button className="callback" onClick={delCallback}>
           <img src={trash} className="callbackSvg" alt="trash" />
         </button>
-        <button className="callback" onClick={delCallback}>
+        <button className="callback" onClick={addCallback}>
           <img src={AddCallback} className="callbackSvg" alt="AddCallback" />
         </button>
       </div>
--- src/TodoHandler.jsx
+++ src/TodoHandler.jsx
@@ -1,45 +1,81 @@
 import React from "react";
 import Add from "./Add.svg";
 import "./TodoHandler.css";
-// import { useState } from "react";
-// import {naoid} from 'naoid'
+import { useState } from "react";
+import { nanoid } from "nanoid";
 
 // const Item = [
 //   {id : naoid(), value : ""}
 // ];
 
 function TodoHandler(props) {
+  const inputRef = React.createRef(null);
 
-  // const [addWords, setaddWords] = useState(addWords);
+  const [words, setWords] = useState([]);
 
-  // const append = (id, value) => {
-  //   const words = [...addWords];
-  //   words.find((item) => item.id === id).value = value;
-  // }
+  const append = (value) => {
+    const item = { id: nanoid(), value, isDone: false, isHidden: false };
+    setWords([...words, item]);
+    inputRef.current.value = "";
+  };
 
-  
+  // const supp = (id) => {
+  //   const wordsCopy =  setWords([...words]);
+  //   const _id = wordsCopy.findIndex(value => value.id === id);
+  //   wordsCopy.splice(_id, 1);
+  //   setWords([...wordsCopy]);
+  // };
 
-    return (
-      <div>
-        <div className="bloc" style={{backgroundColor: `${props.NewbackgroundColor}`}}>
+  const checkbox = (id) => {
+    const wordsCopy = setWords([...words]);
+    const item = wordsCopy.find((value) => value.id === id);
+    item.isDone = !item?.isDone;
+    setWords([...wordsCopy]);
+    console.log(wordsCopy);
+  };
+
+  return (
+    <div>
+      <div
+        className="bloc"
+        style={{ backgroundColor: `${props.NewbackgroundColor}` }}
+      >
         <div className="inputButton">
-          <input maxlength="12"></input>
-          <button className="add">
-            <img
-              src={Add}
-              className="addSvg"
-              alt="AddCallback"
-            />
+          <input maxlength="12" ref={inputRef} />
+          <button
+            className="add"
+            onClick={() => append(inputRef.current.value)}
+          >
+            <img src={Add} className="addSvg" alt="AddCallback" />
           </button>
         </div>
       </div>
-      <div className="content-list">
-
+      <div className="bloc2">
+        {words.map((word) => (
+          <label class="element">
+            <p
+              class="text"
+              style={{
+                position: "relative",
+                textDecoration: `${word.isDone ? "line-through" : "none"}`,
+                // opacity:`${word.isDone ? 0.2 : 1}`
+              }}
+            >
+              <input
+                class="coche"
+                type="checkbox"
+                checked={word.isDone}
+                onChange={() => checkbox(word.id)}
+              />
+              <span class="checkmark"></span>
+              {word.value}
+              {/* <button class="supp" onClick={() => supp(words.id)}>X</button> */}
+            </p>
+          </label>
+        ))}
       </div>
-      </div>
+    </div>
+  );
+}
 
-    );
-  }
-  
-  export default TodoHandler;
-  
+export default TodoHandler;
