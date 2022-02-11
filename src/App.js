80052649a98857ae8397858c000cff19db7cc7e3
Sushib08 <94189204+Sushib08@users.noreply.github.com>
2022-02-11 17:10:24 +0100

    emotion

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import React, { useState } from "react";
 import trash from "./trash.svg";
 import AddCallback from "./AddCallback.svg";
 import TodoHandler from "./TodoHandler";
+import styled from '@emotion/styled'
+/** @jsx jsx */
+import { jsx, css } from '@emotion/react'
 
 function App(props) {
 
@@ -16,13 +19,41 @@ function App(props) {
     setisConnected(() => true);
   };
 
-  let [isColor, setisColor] = useState("#f5f5f5")
+  let [isColor, setisColor] = useState("#f5f5f5");
+
+  const ButtonRed = styled.button` 
+  display: flex;
+  background-color: #ff1b1b;
+  padding: 25px;
+  margin-top: auto;
+  margin-bottom: auto;
+  border-radius: 150px;
+  border: none;
+  width: 30px;
+  height: 30px;;
+`
+const Basic = ({ className }) => (
+  <button className={className} onClick={() => setisColor('#1be8ff')}></button>
+)
+
+const Blue = styled(Basic)`
+  display: flex;
+  background-color: #1be8ff;
+  padding: 25px;
+  margin-top: auto;
+  margin-bottom: auto;
+  margin-left: 5px;
+  border-radius: 150px;
+  border: none;
+`
 
   return (
     <div className="container">
       <div className="colors">
-        <button className="red"  onClick={() => setisColor('#ff1b1b')}></button>
-        <button className="blue" onClick={() => setisColor('#1be8ff')}></button>
+        <ButtonRed  onClick={() => setisColor('#ff1b1b')}></ButtonRed>
+        {/* Styling elements and components */}
+        <Blue />
+        {/* Style any component */}
         <button className="green" onClick={() => setisColor('#1bff49')}> </button>
         <button className="yellow" onClick={() => setisColor('#ffef1b')}></button>
       </div>
