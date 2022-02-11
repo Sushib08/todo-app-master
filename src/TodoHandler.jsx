a3c0505b4a1eb597bbca1085d93d6001836d2959
Sushib08 <94189204+Sushib08@users.noreply.github.com>
2022-02-11 15:19:47 +0100

    feat(Todo app close):

--- src/TodoHandler.jsx
+++ src/TodoHandler.jsx
@@ -11,37 +11,80 @@ import { nanoid } from "nanoid";
 function TodoHandler(props) {
   const inputRef = React.createRef(null);
 
-  const [words, setWords] = useState([]);
+  let [words, setWords] = useState([]);
+
+  const word = words.length;
 
   const append = (value) => {
-    const item = { id: nanoid(), value, isDone: false, isHidden: false };
+    if(value.trim()){
+    const item = { id: nanoid(), value, isDone: false, isHidden: false};
     setWords([...words, item]);
     inputRef.current.value = "";
+    }
   };
 
-  // const supp = (id) => {
-  //   const wordsCopy =  setWords([...words]);
-  //   const _id = wordsCopy.findIndex(value => value.id === id);
-  //   wordsCopy.splice(_id, 1);
-  //   setWords([...wordsCopy]);
-  // };
+  const supp = (id) => {
+    const _id = words.findIndex((value) => value.id === id);
+    words.splice(_id, 1);
+    setWords([...words]);
+  };
+
+  const suppAllChecked =() =>{
+    const wordsCopy = words.filter(item => item.isDone !==true);
+    setWords([...wordsCopy])
+  }
 
   const checkbox = (id) => {
-    const wordsCopy = setWords([...words]);
-    const item = wordsCopy.find((value) => value.id === id);
+    // const wordsCopy = setWords([...words]);
+    const item = words.find((value) => value.id === id);
     item.isDone = !item?.isDone;
+    setWords([...words]);
+    console.log("check", item);
+  };
+
+  const All = () => {
+    const wordsCopy = words.map(item => {
+      item.isHidden = false;
+      return item;
+    });
     setWords([...wordsCopy]);
-    console.log(wordsCopy);
+    console.log(wordsCopy)
+  };
+
+  const active = () => {
+    const wordsCopy = words.map(item => {
+      if (item.isDone) {
+        item.isHidden = true;
+        return item;
+      }
+      item.isHidden = false;
+      return item;
+    });
+    setWords([...wordsCopy]);
+    console.log(wordsCopy)
+  };
+
+  const completed = () => {
+    const wordsCopy = words.map(item => {
+      if (item.isDone) {
+        item.isHidden = false;
+        return item;
+      }
+      item.isHidden = true;
+      return item;
+    });
+    setWords([...wordsCopy]);
+    console.log(wordsCopy)
   };
 
   return (
-    <div>
+    <div className="contain">
       <div
         className="bloc"
         style={{ backgroundColor: `${props.NewbackgroundColor}` }}
       >
         <div className="inputButton">
-          <input maxlength="12" ref={inputRef} />
+          <input maxLength="12" ref={inputRef} />
           <button
             className="add"
             onClick={() => append(inputRef.current.value)}
@@ -52,29 +95,55 @@ function TodoHandler(props) {
       </div>
       <div className="bloc2">
         {words.map((word) => (
-          <label class="element">
+          <label className="element">
             <p
-              class="text"
+              className="text"
               style={{
                 position: "relative",
-                textDecoration: `${word.isDone ? "line-through" : "none"}`,
-                // opacity:`${word.isDone ? 0.2 : 1}`
+                textDecoration: word.isDone ? "line-through" : "none",
+                opacity: word.isDone ? 0.2 : 1,
+                display: word.isHidden ? 'none' : 'block'
               }}
             >
               <input
-                class="coche"
+                className="coche"
                 type="checkbox"
                 checked={word.isDone}
                 onChange={() => checkbox(word.id)}
               />
-              <span class="checkmark"></span>
+              <span className="checkmark"></span>
               {word.value}
-              {/* <button class="supp" onClick={() => supp(words.id)}>X</button> */}
+              <button className="supp" onClick={() => supp(word.id)}>
+                X
+              </button>
             </p>
           </label>
         ))}
       </div>
-    </div>
+      <div className="bloc3">
+        <p className="nbItems">
+          <strong>{word}</strong> items left
+        </p>
+        <div className="filtres">
+          <button className="filtre" onClick={All}>
+            All <strong>{word}</strong>
+          </button>
+          <button className="filtre" onClick={active}>
+            Active
+            <strong>
+               {words.filter((item) => item.isDone === false).length}
+            </strong>
+          </button>
+          <button className="filtre" onClick={completed}>
+            Completed
+            <strong>
+               {words.filter((item) => item.isDone === true).length}
+            </strong>
+          </button>
+        </div>
+        <button className="clear" onClick={suppAllChecked}>Clear completed</button>
+      </div>
+      </div>
   );
 }
 
